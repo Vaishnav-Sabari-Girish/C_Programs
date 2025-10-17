c08978df843a433976055a3f9fd95a48a6cf73c3
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-17 14:47:21 +0530

    Updated logical operators

--- Simply_C/operators/logical_operators/main.c
+++ Simply_C/operators/logical_operators/main.c
@@ -1,6 +1,33 @@
+// Logical operators allow us to manipulate data via logic gates (AND, OR, NOT etc)
+/*
+ * The logical operators include
+ * Logical AND (&&)
+ * Logical OR (||)
+ * Logical NOT (!)
+*/ 
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  int x = 10;
+  int y = 10;
+
+  if (10 == x && 9 == y) {
+    printf("Both are True\n");   // Won't enter this since one is false
+  } else {
+    printf("One is False\n");
+  }
+
+  if (10 == x || x != y) {
+    printf("One is True\n");    // Enters here since one is true
+  } else {
+    printf("Both are false\n");
+  }
+
+  if (!x == !y && x == y) {
+    printf("Both are True\n");
+  } else {
+    printf("Both are false\n");
+  }
+  return 0;
 }
