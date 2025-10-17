ebbfcc5edb48fc932913dc08c54bb2fb160a2876
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-17 21:08:36 +0530

    Added If statements

---
+++ Simply_C/decisions/decisions.md
@@ -0,0 +1,4 @@
+# Decisions in C
+
+1. [If Statements](./if_statements/main.c)
+2. [Switch Statements](./switch_statements/main.c)
---
+++ Simply_C/decisions/if_statements/main.c
@@ -0,0 +1,15 @@
+// if statements are used to make decisions
+// It checks if a given statement is true or false
+
+#include <stdio.h>
+
+int main() {
+  int x = 10, y = 20;
+
+  if (x + y == 30) {
+    printf("x + y = 30\n");
+  } else {
+    printf("x + y != 30\n");
+  }
+  return 0;
+}
