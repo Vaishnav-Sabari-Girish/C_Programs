e62956d630ba521ff09fdd446927d86eef311233
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-17 21:12:50 +0530

    Added Switch statements

--- Simply_C/decisions/switch_statements/main.c
+++ Simply_C/decisions/switch_statements/main.c
@@ -1,6 +1,28 @@
+// Switch statements are used when the expresion used to make the decision can have multiple outputs
+
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  int x = 10, y = 40;
+  int z = x + y;
+
+  switch (z) {
+    case 20 : 
+      printf("x + y = 20\n");
+      break;
+    case 30 : 
+      printf("x + y = 30\n");
+      break;
+    case 40:
+      printf("x + y = 40\n");
+      break;
+    case 50:
+      printf("x + y = 50\n");
+      break;
+    default:
+      printf("Invalid Value\n");
+      break;
+  }
+  return 0;
 }
