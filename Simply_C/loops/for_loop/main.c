dadb20e19c0d1d27be9836c6fef19ecef936824e
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-17 21:55:52 +0530

    Added For Loop program

--- Simply_C/loops/for_loop/main.c
+++ Simply_C/loops/for_loop/main.c
@@ -1,6 +1,11 @@
+// For loops are used to perform an operation within a given range
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+  for (int i = 0; i <= 10; i++) {
+    printf("%d\n", i);
+  }
+  return 0;
 }
