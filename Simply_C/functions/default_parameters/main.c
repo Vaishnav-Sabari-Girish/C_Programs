2f869cf5e62753ef2d81323618766f5be8ef84e8
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-18 10:53:37 +0530

    Added default parameters

--- Simply_C/functions/default_parameters/main.c
+++ Simply_C/functions/default_parameters/main.c
@@ -1,6 +1,16 @@
+// Functions can have default parameters too.
+// When we give the values to those parameters the function is executed with those parameters
+
 #include <stdio.h>
 
+int sum(int a, int b) {
+  return a + b;
+}
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  int a = 10, b = 65;
+  int out = sum(a, b);
+
+  printf("Sum of %d and %d is %d\n", a, b, out);
+  return 0;
 }
