23d627f3dc811c5d09cbfe3816de00d22a5c33d9
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-18 10:20:34 +0530

    Added Do-While Loop

--- Simply_C/loops/do_while_loop/main.c
+++ Simply_C/loops/do_while_loop/main.c
@@ -1,6 +1,13 @@
+// Do-While loops are similar to while loops
+// The only difference is that the loop first executes the statement and then looks into the logic
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  int i = 0;
+  do {
+    printf("%d\n", i);
+    i++;
+  } while (i == 0);    // Executes the do section first then checks the while section
+  return 0;
 }
