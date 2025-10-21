11d8f605f5ec13f10cffb4c9aeee6bb11523afc4
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-21 12:43:56 +0530

    Added array processing

--- Simply_C/arrays/array_processing/main.c
+++ Simply_C/arrays/array_processing/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+
 int main() {
-  printf("Hello, World!\n");
+  int i = 0;
+  int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  int len = sizeof(a) / sizeof(a[0]);
+
+  printf("Original Array : \n\n");
+
+  for (int j = 0; j < len; j++) {
+    printf("%d ", a[j]);
+  }
+
+  printf("\n\n");
+
+  while (i < len) {
+    a[i] *= 10;
+    i++;
+  }
+
+  printf("Modified array : \n\n");
+
+  for (int k = 0; k < len; k++) {
+    printf("%d ", a[k]);
+  }
+
+  printf("\n");
   return 0;
 }
