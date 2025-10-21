82c4e82d5db8fcb883ddd934b00ce9745d3086ee
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-21 11:31:27 +0530

    Added multidimensional array

--- Simply_C/arrays/multi_dimensional_arrays/main.c
+++ Simply_C/arrays/multi_dimensional_arrays/main.c
@@ -1,6 +1,22 @@
+// Multi-dimensional arrays are basically a collection of arrays inside an array
+
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!\n");
+  int ma[3][2] = {
+    {1, 2}, 
+    {3, 4}, 
+    {5, 6}
+  };  
+
+  int rows = sizeof(ma) / sizeof(ma[0]);   // Get number of rows
+  int cols = sizeof(ma[0]) / sizeof(ma[0][0]);  // Size of 1 row  + size of one element in that row
+  // Printing the Multi-dimensional array
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      printf("%d ", ma[i][j]);
+    }
+    printf("\n");
+  }
   return 0;
 }
