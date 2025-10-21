beb0264b9962472e8faa4c5d75261da92898df65
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-10-21 12:56:05 +0530

    Added string

--- Simply_C/arrays/strings/main.c
+++ Simply_C/arrays/strings/main.c
@@ -1,6 +1,10 @@
+// Strings are an array of characters and are teminated with a \0 in the end automatically.
+
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!\n");
+  char str[] = "This is a string";
+
+  printf("String : %s\n", str);
   return 0;
 }
